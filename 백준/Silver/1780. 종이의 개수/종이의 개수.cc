e8b82eb8d357dfc00cb96e8d657c1aa6e7120e50
Usang Lee <krkr728@gmail.com>
2024-03-10 19:09:22 +0900

    [Silver II] Title: 종이의 개수, Time: 1100 ms, Memory: 20720 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -6,35 +6,34 @@ int grid[2188][2188];
 int N;
 int cnt[3];
 
-void recv(int y, int x, int size) {
-//    cout << y << " " << x << " " << size << endl;
+int recv(int y, int x, int size) {
     int target = grid[y][x];
-    bool all = true;
+
+    if (size == 1) {
+        cnt[target + 1]++;
+        return target;
+    }
 
     int end_y = y + size;
     int end_x = x + size;
+    size /= 3;
 
-    for (int i = y; i < end_y; ++i) {
-        for (int j = x; j < end_x; ++j) {
-            if (grid[i][j] != target) {
+    bool all = true;
+
+    for (int i = y; i < end_y; i += size) {
+        for (int j = x; j < end_x; j += size) {
+            if (recv(i, j, size) != target) {
                 all = false;
-                break;
             }
         }
     }
 
     if (all) {
-        cnt[target + 1] += 1;
-        return;
+        cnt[target + 1] -= 8;
+        return target;
     }
 
-    size /= 3;
-
-    for (int i = y; i < end_y; i += size) {
-        for (int j = x; j < end_x; j += size) {
-            recv(i, j, size);
-        }
-    }
+    return -2;
 }
 
 int main() {
