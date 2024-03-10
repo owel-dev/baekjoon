4957881c45c4d3901b9f4ac2a3fafbf4ff0dda03
Usang Lee <krkr728@gmail.com>
2024-03-10 21:49:58 +0900

    [Silver II] Title: 종이의 개수, Time: 68 ms, Memory: 21044 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,52 +1,56 @@
-#include <iostream>
+#include<cstdio>
 
-using namespace std;
+char buffer[1 << 20];
+int idx, ridx;
 
-int grid[2188][2188];
-int N;
-int cnt[3];
-
-int recv(int y, int x, int size) {
-    int target = grid[y][x];
-
-    if (size == 1) {
-        cnt[target + 1]++;
-        return target;
+static inline char read() {
+    if (idx == ridx) {
+        ridx = fread(buffer, 1, 1 << 20, stdin);
+        idx = buffer[ridx] = 0;
     }
+    return buffer[idx++];
+}
 
-    int end_y = y + size;
-    int end_x = x + size;
-    size /= 3;
+static inline int readint() {
+    int r = 0;
+    bool n = 0;
+    register char c = read();
+    while (c < 33) c = read();
+    if (c == '-') n = 1, c = read();
+    while (c >= 48 && c <= 57) {
+        r = r * 10 + (c & 15);
+        c = read();
+    }
+    return n ? -r : r;
+}
 
-    bool all = true;
+int N, p[2200][2200], res[3];
 
-    for (int i = y; i < end_y; i += size) {
-        for (int j = x; j < end_x; j += size) {
-            if (recv(i, j, size) != target) {
-                all = false;
+int numberOfPaper(int y, int x, int n) {
+    if (n == 1) {
+        res[p[y][x] + 1]++;
+        return p[y][x];
+    }
+    bool isOne = 1;
+    for (int i = 0; i < n; i += n / 3) {
+        for (int j = 0; j < n; j += n / 3) {
+            if (numberOfPaper(y + i, x + j, n / 3) != p[y][x]) {
+                isOne = 0;
             }
         }
     }
-
-    if (all) {
-        cnt[target + 1] -= 8;
-        return target;
+    if (isOne) {
+        res[p[y][x] + 1] -= 8;
+        return p[y][x];
     }
-
-    return -2;
+    else return 2;
 }
 
 int main() {
-    cin >> N;
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            cin >> grid[i][j];
-        }
-    }
-
-    recv(0, 0, N);
-
-    for (int i: cnt) {
-        cout << i << endl;
-    }
+    N = readint();
+    for (int i = 0; i < N; ++i)
+        for (int j = 0; j < N; ++j)
+            p[i][j] = readint();
+    numberOfPaper(0, 0, N);
+    printf("%d\n%d\n%d", res[0], res[1], res[2]);
 }
