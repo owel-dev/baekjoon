c2b57a279b680f3321afa3b4eceec314dc884fed
Usang Lee <krkr728@gmail.com>
2024-03-10 18:29:07 +0900

    [Silver II] Title: 종이의 개수, Time: 1072 ms, Memory: 20720 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.cc
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.cc
@@ -1,27 +1,25 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int grid[2188][2188];
-
 int N;
-
 int cnt[3];
 
-int c;
-void recv(int y1, int x1, int y2, int x2) {
-    c++;
-    int target = grid[y1][x1];
+void recv(int y, int x, int size) {
+//    cout << y << " " << x << " " << size << endl;
+    int target = grid[y][x];
     bool all = true;
 
-    for (int i = y1; i <= y2; ++i) {
-        for (int j = x1; j <= x2; ++j) {
+    int end_y = y + size;
+    int end_x = x + size;
+
+    for (int i = y; i < end_y; ++i) {
+        for (int j = x; j < end_x; ++j) {
             if (grid[i][j] != target) {
                 all = false;
                 break;
             }
-
         }
     }
 
@@ -30,11 +28,11 @@ void recv(int y1, int x1, int y2, int x2) {
         return;
     }
 
-    int gap = (y2 - y1) / 3;
+    size /= 3;
 
-    for (int i = y1; i <= y2; i += gap + 1) {
-        for (int j = x1; j <= x2; j += gap + 1) {
-            recv(i, j, i + gap, j + gap);
+    for (int i = y; i < end_y; i += size) {
+        for (int j = x; j < end_x; j += size) {
+            recv(i, j, size);
         }
     }
 }
@@ -47,9 +45,9 @@ int main() {
         }
     }
 
-    recv(0, 0, N - 1, N - 1);
+    recv(0, 0, N);
 
-    for (int i : cnt) {
+    for (int i: cnt) {
         cout << i << endl;
     }
 }
