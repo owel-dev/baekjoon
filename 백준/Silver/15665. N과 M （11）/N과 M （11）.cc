16b2f14571d1e6621a7bbe9b9a44290cfe248870
Usang Lee <krkr728@gmail.com>
2024-03-28 22:48:18 +0900

    [Silver II] Title: N과 M (11), Time: 204 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+
+int seq[9], ans[9];
+
+void recv(int idx) {
+    if (idx == M) {
+        for (int i = 0; i < M; ++i) {
+            cout << ans[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    int dup_check = 0;
+    for (int i = 0; i < N; ++i) {
+        if (seq[i] != dup_check) {
+            dup_check = seq[i];
+            ans[idx] = seq[i];
+            recv(idx + 1);
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    for (int i = 0; i < N; ++i) {
+        int num;
+        cin >> num;
+        seq[i] = num;
+    }
+    sort(seq, seq + N);
+    recv(0);
+}
